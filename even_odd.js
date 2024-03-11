f6451cf74f95f5e0bf65efcc81323998880c4541
Muhiu-DDin <muhiuddin554433@gmail.com>
2024-03-11 21:56:04 +0500

    JS assigment

---
+++ even_odd.js
@@ -0,0 +1,125 @@
+// let num = +prompt("enter your number");
+// if(num%2==0){
+//     console.log("even number");
+//     document.write("even number");
+// }
+// else{
+//     console.log("odd number");
+//     document.write("odd number");
+// }
+
+// -------------------------------------------------------------
+
+// let num = +prompt("enter your number");
+// if(num<0){
+//     console.log("negative number");
+// }
+// else if(num > 0 ){
+//     console.log("positive number");
+// }
+// else{
+//     console.log("number is zero");
+// }
+
+// ---------------------------------------------------------------------
+
+// let num1 = +prompt("enter your 1st number");
+// let num2 = +prompt("enter your 2nd number");
+
+// if(num1>num2){
+//     console.log(`${num1} is greater than ${num2}`);
+// }
+// else if (num2>num1){
+//     console.log(`${num2} is greater than ${num1}`);
+// }
+// else{
+//     console.log(`${num1} is equal to ${num2}`);
+// }
+
+// -----------------------------------------------------------------------
+
+// let num1 = +prompt("enter your 1st sub number");
+// let num2 = +prompt("enter your 2nd sub number");
+// let num3 = +prompt("enter your 3rd sub number");
+
+// let average = (num1+num2+num3)/3;
+
+// if (average >=90){
+//     console.log("A++");
+// }
+// else if(average >=80){
+//     console.log("A+");
+// }
+// else if(average >=70){
+//     console.log("A");
+// }
+// else if(average >=60){
+//     console.log("B");
+// }
+// else{
+//     console.log("below average");
+// }
+
+// ----------------------------------------------------------------------------------------
+
+// let TC = +prompt("enter temperature value in C");
+
+// function convertToF(TC){
+
+// let TF = (TC * 9/5)+32;
+// console.log(TF);
+// }
+
+// convertToF(TC);
+
+// --------------------------------------------------------------------------------------------
+
+// let side1 = +prompt("enter side 1");
+// let side2 = +prompt("enter side 2");
+// let side3 = +prompt("enter side 3");
+
+// if(side1==side2 || side2==side3 || side1==side3){
+//     console.log("the given triangle is isoceles");
+// }
+// else if(side1==side==side3){
+//     console.log("the given triangle is equivalent");
+// }
+// else if(side1!=side2 & side2!=side3 & side1!=side3){
+//     console.log("the given triangle is scalene");
+// }
+
+// ---------------------------------------------------------------------------------------------
+
+// let char = prompt("enter the character");
+// if(char==`a`|| char==`e`|| char==`i`|| char==`o`|| char==`u`){
+//     console.log("your character is vowel");
+// }
+// else{
+//     console.log("your character is consonant");
+// }
+
+// ---------------------------------------------------------------------------------------------
+
+// let base = +prompt("enter base number");
+// let exponent = +prompt("enter exponent");
+
+// let power = base**exponent;
+// console.log(power);
+
+// ---------------------------------------------------------------------------------------------
+
+// let age = +prompt("enter you age");
+// if(age>=1 && age<=12){
+//     console.log("you are a child");
+// }
+// else if(age>=13 && age<=19){
+//     console.log("you are a teenager");
+// }
+// else if(age>=20 && age<=64){
+//     console.log("you are an adult");
+// }
+// else if(age>=65){
+//     console.log("you are a senior");
+// }
+
+// --------------------------------------------------------------------------------------------
